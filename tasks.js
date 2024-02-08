3b377e5eafdb41f2d47941b9e21d017fc67c48ab
hawraa473 <hawraamuayad473@gmail.com>
2024-02-08 17:51:24 +0300

    js-trying

--- tasks.js
+++ tasks.js
@@ -1,73 +1,73 @@
-// Task 1
-function checkVotingEligibility() {
-  var myAge = 18
-if ( myAge => 18 ) {
-console.log( "You are eligible for voting,") ;
-} else { 
-console.log( "You are not eligible for voting." ) ; 
-}
-}
-checkVotingEligibility();
+ 
+ //Task 1
 
-
-// Task 2
-function printNumbersWithWhile() {
-  var i = 1 ;
- while ( i < 6 ) {
-console.log(i);
- i++;
-} 
-}
-printNumbersWithWhile();
-
-
-// Task 3
-
-function printEvenNumbersWithFor(i) {
-  for ( var i = 2 ; i  <12 ; i+2 ) {
-  console.log( i );
- }
- }
-printEvenNumbersWithFor();
-
-// Task 4
-function getDayOfWeek() { 
-  var a=1 ;
- switch(a){
-case 1 : 
-console.log ( ' Saturday ')
-break;  
-case 2 : 
-console.log ( ' Sunday ')
-break;  
-case 3 : 
-console.log ( ' Monday ')
-break;  
-case 4 : 
-console.log ( ' Tuesday ')
-break;  
-case 5 : 
-console.log ( ' Wednesday ')
-break;  
-case 6 : 
-console.log ( ' Thursday ')
-break;  
-case 7 : 
-console.log ( ' Friday ')
-break;  
-}
-}
-getDayOfWeek();
-// Task 5
-function sum( ) { 
-  var a= 'Zahraa' ;
-  var b= " Hawraa " ;
-console.log( a + b ) ;
-} 
- sum();
-
-// Task 6
-//Uncomment and solve
-var fruits = [ 'orange'  , 'apple' , 'banana' ] ;
-console.log( fruits ) ;
-fruits 
+ function checkVotingEligibility () {
+  var myAge = 18 ;
+  if( myAge >= 18 ) {
+      console.log("You are eligible for voting,");
+  } 
+  else{
+      console.log(
+         "You are not eligible for voting." 
+      );
+  }
+    }
+    checkVotingEligibility(); 
+  
+    //Task 2
+  
+    function printNumbersWithWhile(){
+      var i=1 ;
+      while(i<6 ){console.log(i);
+          i++;
+      }
+    }
+    printNumbersWithWhile();
+  
+  
+    //Task 3
+  function printEvenNumbersWithFor (){
+      for (let i = 2; i <= 11; i += 2) {
+      console.log(i);
+  }
+  }
+  printEvenNumbersWithFor();
+   
+  //Task 4
+  
+  function getDayOfWeek(){
+      let day = 3;
+  let dayName;
+  
+  switch (day) {
+    case 1:
+      dayName = "Sunday";
+      break;
+    case 2:
+      dayName = "Monday";
+      break;
+    // Add cases for other days...
+    default:
+      dayName = "Invalid day";
+  }
+  console.log("Today is " + dayName); 
+  }
+  getDayOfWeek();
+  
+  //Task 5
+  
+  function sum( ) { 
+      var a= 1 ;
+      var b= 2 ;
+    console.log( a + b ) ;
+    } 
+     sum(); 
+  
+     //Task 6 
+     
+     var fruits = [ 'orange'  , 'apple' , 'banana' ] ;
+  console.log( fruits ) ; 
+  
+  
+  
+  
